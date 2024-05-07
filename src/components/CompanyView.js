06d22177c5415a908a4697dd8c3f5bec43750b5f
Paul Marina-Incefi <whitedrakkar@gmail.com>
2024-05-07 21:41:32 +0300

    Handle Upload and Validate for anaf operations on company view -> invoices list

--- src/components/CompanyView.js
+++ src/components/CompanyView.js
@@ -47,6 +47,54 @@ export function CompanyView({ companyId }) {
         navigate(path, { replace: true });
     }
 
+    function handleValidate(invoice) {
+        setLoading(true);
+        validateInAnaf(invoice, (result) => {
+            invoice.anafStatus.status = result.successful ? 'ValidatedOk' : 'ValidatedFail';
+            setLoading(false);
+        });
+    }
+
+    const validateInAnaf = async (invoice, refreshResult) => {
+        try {
+            const response = await fetch(new Request("http://127.0.0.2:8080/api/invoices/" + invoice.id + "/upload/validate", {
+                method: "PATCH",
+                headers: {
+                    'Accept': 'application/json',
+                }
+            }));
+
+            const json = await response.json();
+            refreshResult(json);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    function handleUpload(invoice) {
+        setLoading(true);
+        uploadToAnaf(invoice, (result) => {
+            invoice.anafStatus.status = result.successful ? 'UploadedOk' : 'UploadedFail';
+            setLoading(false);
+        });
+    }
+
+    const uploadToAnaf = async (invoice, refreshResult) => {
+        try {
+            const response = await fetch(new Request("http://127.0.0.2:8080/api/invoices/" + invoice.id + "/upload", {
+                method: "PATCH",
+                headers: {
+                    'Accept': 'application/json',
+                }
+            }));
+
+            const json = await response.json();
+            refreshResult(json);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
         <>
             <div>
@@ -65,16 +113,29 @@ export function CompanyView({ companyId }) {
 
             <div>Invoices</div>
             <div className='container'>
+                <div className='row' key='headers'>
+                    <div className='col-sm'>Number</div>
+                    <div className='col-sm'>Issue date</div>
+                    <div className='col-3'>From</div>
+                    <div className='col-3'>To</div>
+                    <div className='col-sm'>Amount</div>
+                    <div className='col-sm'>Anaf status</div>
+                    <div className='col-sm'>Operations</div>
+                </div>
                 {
                     invoices.map((invoice, index) => {
                         return (
                             <div className='row' key={index}>
                                 <div className='col-sm'>{invoice.number}</div>
                                 <div className='col-sm'>{invoice.issueDate}</div>
-                                <div className='col-sm'>{invoice.dueDate}</div>
                                 <div className='col-3'>{invoice.supplier.name}</div>
                                 <div className='col-3'>{invoice.customer.name}</div>
                                 <div className='col-sm'>{invoice.totalAmount} {invoice.currency}</div>
+                                <div className='col-sm'>{invoice.anafStatus?.status}</div>
+                                <div className='col-sm'>
+                                    {(invoice.anafStatus == null || invoice.anafStatus?.status === 'None' || invoice.anafStatus?.status === 'UploadedFail') && <button onClick={() => handleUpload(invoice)}>Upload</button>}
+                                    {(invoice.anafStatus?.status === 'UploadedOk' || invoice.anafStatus?.status === 'ValidatedFail') && <button onClick={() => handleValidate(invoice)}>Validate</button>}
+                                </div>
                             </div>
                         );
                     })
